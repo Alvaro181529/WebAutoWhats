e16bf3d0c8ab07609ac9ce8faa74eb31189948b9
Alvaro181529 <aljak1815@gmail.com>
2024-02-16 12:24:34 -0400

    te

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ const conn = createPool({
   host: "172.65.10.50",
   port: 3306,
   database: "trackpak",
-  user: "root",
+  user: "agbc",
   password: "Correos.2023",
 });
 module.exports = { conn };
