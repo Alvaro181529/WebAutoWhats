6c3240265f56dc29191411f2831b8c4279e2916f
Alvaro181529 <aljak1815@gmail.com>
2024-02-21 09:21:49 -0400

    cambio hora

--- src/controller/lapaz.js
+++ src/controller/lapaz.js
@@ -34,7 +34,7 @@ exports.lapazController = (req, res) => {
       //lunes , martes, miercoles, 15:30hrs envio
       //jueves, viernes, 15:30hrs reenvio
       if (estado == "conectado") {
-        cron.schedule("0 8 * * 1,2,3,4,5", () => {
+        cron.schedule("25 9 * * 1,2,3,4,5", () => {
           // cron.schedule("23 * * * *", () => {
           comprobacion();
         });
