97153056948683b1b40fda861c0a756944d34135
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-03-15 09:54:06 -0400

    tiempo

--- src/controller/lapaz.js
+++ src/controller/lapaz.js
@@ -36,7 +36,7 @@ exports.lapazController = (req, res) => {
       //lunes , martes, miercoles, 15:30hrs envio
       //jueves, viernes, 15:30hrs reenvio
       if (estado == "conectado") {
-        cron.schedule("53 9 * * 1,2,3,4,5,6", () => {
+        cron.schedule("55 9 * * 1,2,3,4,5,6", () => {
         // cron.schedule("* * * * *", () => {
           comprobacion();
         });
