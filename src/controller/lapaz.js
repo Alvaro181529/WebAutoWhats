d99568e152e0e370a12d70c7e9c5e81ad9c77be4
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-04-01 15:04:57 -0400

    arreglos

--- src/controller/lapaz.js
+++ src/controller/lapaz.js
@@ -34,7 +34,7 @@ exports.lapazController = (req, res) => {
     try {
       const lpl = [{ estado, codigo, contacto, code: src }];
       if (estado == "conectado") {
-        cron.schedule("22 14 * * 1-6", () => {
+        cron.schedule("10 15 * * 1-6", () => {
           comprobacion();
         });
         cron.schedule("0 10 * * 3,6", () => {
