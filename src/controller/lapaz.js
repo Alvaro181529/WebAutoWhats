4c719469b4adb4f0aedfaa11e6352b440030d391
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-04-04 17:32:22 -0400

    update

--- src/controller/lapaz.js
+++ src/controller/lapaz.js
@@ -35,7 +35,7 @@ exports.lapazController = (req, res) => {
       const lpl = [{ estado, codigo, contacto, code: src }];
       if (estado == "conectado") {
 
-        cron.schedule("31 17 * * 1-6", () => {
+        cron.schedule("35 17 * * 1-6", () => {
           comprobacion();
         });
         cron.schedule("0 10 * * 3,6", () => {
--- src/whatsapp/lapaz.js
+++ src/whatsapp/lapaz.js
@@ -15,7 +15,7 @@ async function ClientLP() {
       ],
     },
     authStrategy: new LocalAuth({ clientId: "lapaz" }),
-    webVersionCache: { tipo: 'remoto', remotePath: ' https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2410.1.html ', } 
+    webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', }
   });
 
   lapaz.on("qr", (qr) => {
