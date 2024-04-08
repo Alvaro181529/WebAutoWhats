e091ee32bdb3faea8840a7bec466090b6e5dbc4e
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-04-08 16:04:24 -0400

    actualizacion de hora

--- src/controller/lapaz.js
+++ src/controller/lapaz.js
@@ -35,7 +35,7 @@ exports.lapazController = (req, res) => {
       const lpl = [{ estado, codigo, contacto, code: src }];
       if (estado == "conectado") {
         console.log("esta dentro de la hora")
-        cron.schedule("57 18 * * 1-6", () => {
+        cron.schedule("16 16 * * 1-6", () => {
           console.log("esta aca en comprobacion ")
           comprobacion();
         });
