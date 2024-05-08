37fd3a0952224da212da8808a40874c3012fd363
Alvaro herbert Medrano perez <aljak1815@gmail.com>
2024-05-08 12:07:45 -0400

    Arreglo

--- src/database/db.js
+++ src/database/db.js
@@ -1,22 +1,22 @@
-// const { createPool } = require("mysql2/promise");
-// const conn = createPool({
-//   host: "172.65.10.50",
-//   // host: "172.65.10.52",
-//   port: 3306,
-//   database: "trackpak",
-//   user: "agbc",
-//   password: "Correos.2023",
-//   // user: "agbc",
-//   // password: "agbc",
-// });
-// module.exports = { conn };
-
 const { createPool } = require("mysql2/promise");
 const conn = createPool({
-  host: "localhost",
+  host: "172.65.10.50",
+  // host: "172.65.10.52",
   port: 3306,
   database: "trackpak",
-  user: "root",
-  password: "",
+  user: "agbc",
+  password: "Correos.2023",
+  // user: "agbc",
+  // password: "agbc",
 });
 module.exports = { conn };
+
+// const { createPool } = require("mysql2/promise");
+// const conn = createPool({
+//   host: "localhost",
+//   port: 3306,
+//   database: "trackpak",
+//   user: "root",
+//   password: "",
+// });
+// module.exports = { conn };
